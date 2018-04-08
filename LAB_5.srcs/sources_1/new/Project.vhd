a3eb66dedeffab562adeceb1fde65949eda61061
Fredrik Throndsen Hove <fredrh16@uia.no>
2018-04-08 18:32:22 +0200

    Added components to Project file

--- LAB_5.srcs/sources_1/new/Project.vhd
+++ LAB_5.srcs/sources_1/new/Project.vhd
@@ -1,27 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.04.2018 18:13:18
--- Design Name: 
--- Module Name: Project - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use work.components.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -51,6 +30,14 @@ SPI_master : ELE112_LAB_5
            Reset:in STD_LOGIC; --Reset.
            CLK : in STD_LOGIC; --CLK in from our board.
            SPI_SCLK_sel : in STD_LOGIC_VECTOR(3 downto 0)
+           );
+
+DATA_STORAGE : ele112_regn
+GENERIC MAP( N <= 64)
+    PORT MAP ( R => INPUT,
+           Rin => LD,
+           Clock => CLK,
+           Q => OUTPUT
            );
 
 
